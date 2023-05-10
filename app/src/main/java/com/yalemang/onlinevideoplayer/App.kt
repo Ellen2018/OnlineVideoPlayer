c9a4b3b43fc843bdd1927f261959f8a2c9758378
陈浪 <1961075191@qq.com>
2023-05-10 22:41:35 +0800

    完成m3u8的在线播放功能

--- app/src/main/java/com/yalemang/onlinevideoplayer/App.kt
+++ app/src/main/java/com/yalemang/onlinevideoplayer/App.kt
@@ -1,15 +1,19 @@
 package com.yalemang.onlinevideoplayer
 
 import android.app.Application
+import com.shuyu.gsyvideoplayer.cache.CacheFactory
 import com.shuyu.gsyvideoplayer.player.PlayerFactory
 import tv.danmaku.ijk.media.exo2.Exo2PlayerManager
+import tv.danmaku.ijk.media.exo2.ExoPlayerCacheManager
 
 
 class App:Application() {
 
     override fun onCreate() {
         super.onCreate()
+        //m3u8支持
         PlayerFactory.setPlayManager(Exo2PlayerManager::class.java)
+        CacheFactory.setCacheManager(ExoPlayerCacheManager::class.java)
     }
 
 }
--- app/src/main/java/com/yalemang/onlinevideoplayer/MainActivity.kt
+++ app/src/main/java/com/yalemang/onlinevideoplayer/MainActivity.kt
@@ -5,15 +5,33 @@ import android.os.Bundle
 import com.shuyu.gsyvideoplayer.video.GSYADVideoPlayer
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
 
+/**
+ * 1.m3u8的在线播放
+ * 2.m3u8的下载与播放
+ * 3.m3u8投屏
+ */
 class MainActivity : AppCompatActivity() {
+
+    private var testM3U8Array = arrayOf(
+        "https://ask.dcloud.net.cn/topic/m3u8",//无效
+        "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8",
+        "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-uni4934e7b/c4d93960-5643-11eb-a16f-5b3e54966275.m3u8",//无效
+        "http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8"//无效
+    )
 
     private lateinit var standardGSYVideoPlayer: StandardGSYVideoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onlineM3U8Play()
+    }
+
+    /**
+     * 在线播放
+     */
+    private fun onlineM3U8Play(){
         standardGSYVideoPlayer = findViewById(R.id.video_player)
-        val url = "https://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4"
-        standardGSYVideoPlayer.setUp(url,true,"测试m3u8")
+        standardGSYVideoPlayer.setUp(testM3U8Array[1],true,"测试m3u8")
     }
 }
